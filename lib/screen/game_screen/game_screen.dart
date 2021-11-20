728e9cbb3a5b4be0f6d21cc9a30b3ece887c3f0e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-20 20:58:18 +0900

    add: todo

--- lib/screen/game_screen/game_screen.dart
+++ lib/screen/game_screen/game_screen.dart
@@ -7,6 +7,8 @@ import 'package:prize_get_app/screen/game_screen/game_screen_controller.dart';
 class GameScreen extends StatelessWidget {
   const GameScreen({Key? key}) : super(key: key);
 
+  //TODO 1回無敵アイテムをつける　＝＞flutter pub add slider_button
+
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(GameScreenController(), tag: '');

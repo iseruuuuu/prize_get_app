9fb398e1232440d01b1f0ee4acf8c05e893150b0
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-20 18:14:21 +0900

    add: スタートの際のダイアログを追加

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:prize_get_app/screen/home/home_screen_controller.dart';
 
@@ -8,45 +9,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(HomeScreenController(), tag: '');
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+    return FlutterEasyLoading(
+      child: Scaffold(
         backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text(
-              'GET OR FINISH',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            TextButton(
-              onPressed: controller.onTap,
-              child: const Text(
-                'スタート',
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'GET OR FINISH',
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.cyan,
-                ),
-              ),
-            ),
-            Obx(() => Text(
-                'High Score: ${controller.highScore}',
-                style: const TextStyle(
-                  fontSize: 30,
+                  fontSize: 40,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ),
               ),
-            ),
-          ],
+              TextButton(
+                onPressed: controller.onTap,
+                child: const Text(
+                  'スタート',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.cyan,
+                  ),
+                ),
+              ),
+              Obx(() => Text(
+                  'High Score: ${controller.highScore}',
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/home/home_screen_controller.dart
+++ lib/screen/home/home_screen_controller.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:prize_get_app/preference/shared_preference.dart';
 import 'package:prize_get_app/screen/game_screen/game_screen.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:open_settings/open_settings.dart';
+
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class HomeScreenController extends GetxController {
   var highScore = 0.obs;
@@ -10,6 +13,7 @@ class HomeScreenController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    _sharedPreference();
   }
 
   void _sharedPreference() async {
@@ -17,6 +21,17 @@ class HomeScreenController extends GetxController {
   }
 
   void onTap() {
-    Get.to(() => const GameScreen());
+    double _progress = 0;
+    Timer? _timer;
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (Timer timer) {
+      EasyLoading.showProgress(_progress,
+          status: '${(_progress * 100).toStringAsFixed(0)}%');
+      _progress += 0.03;
+      if (_progress >= 1) {
+        _timer?.cancel();
+        EasyLoading.dismiss();
+        Get.to(() => const GameScreen());
+      }
+    });
   }
 }

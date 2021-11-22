ae0cd536cbba34ac8bdf7be711f64901d484ae32
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-11-22 21:52:30 +0900

    add: homeのアニメーションを載せた。

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animator/flutter_animator.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:prize_get_app/screen/home/home_screen_controller.dart';
@@ -24,26 +25,35 @@ class HomeScreen extends StatelessWidget {
               const Text(
                 'GET OR FINISH',
                 style: TextStyle(
-                  fontSize: 40,
+                  fontSize: 45,
                   fontWeight: FontWeight.bold,
                   color: Colors.green,
                 ),
               ),
-              TextButton(
-                onPressed: controller.onTap,
-                child: const Text(
-                  'スタート',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
+              Pulse(
+                preferences: const AnimationPreferences(
+                  autoPlay: AnimationPlayStates.Loop
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  height: MediaQuery.of(context).size.width / 4,
+                  child: ElevatedButton(
+                    onPressed: controller.onTap,
+                    child: const Text(
+                      'スタート',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
               ),
               Obx(() => Text(
                   'High Score: ${controller.highScore}',
                   style: const TextStyle(
-                    fontSize: 30,
+                    fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Colors.green,
                   ),
